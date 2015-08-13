c5061a299be390ce05da821ffd6fe1b9d30954e0
ScorpMarv <marv1336@gmail.com>
2015-08-13 10:43:53 -0300

    Removed debug info on calendar

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -10,26 +10,19 @@
 
         if (actividades && (actividades.length != 0)) {
             loadCalendar();
-            console.log('if');
-            $("#debug").append('<div>IF</div>');
         } else {
-            console.log('else');
-            $("#debug").append('<div>ELSE</div>');
             $.ajax({
                 url: "http://acquavel.herokuapp.com/api/actividades/?format=json",
                 dataType: "json",
                 async: true,
                 success: function (result) {
-                    $("#debug").append('<div>SUCCESS</div>');
                     var actividadesJSON = JSON.stringify(result);
                     localStorage.setItem('actividades', actividadesJSON);
-                    console.log(result);
                     actividades = result;
                     loadCalendar();
                 },
                 error: function (xhr, ajaxOptions, thrownError) {
                     console.log("errorstatus: " + xhr.status + " ajaxoptions: " + ajaxOptions + " throwError: " + thrownError);
-                    $("#debug").append('<div>ERROR</div>');
                 }
             });
         }
@@ -51,12 +44,11 @@
     function loadCalendar(){
         $.each(actividades, function (key,value){
             var description = value.descripcion;
-            console.log('Descripcion: ' + description);
+            console.log(description);
             $.each(value.horarios, function (key, value){
                 var day = value.dia.toLowerCase();
                 var startHour = parseInt(value.desdehora);
                 var endHour = parseInt(value.hastahora);
-                console.log('Dia: ' + day + '  Hora 1: ' + startHour + '  Hora 2: ' + endHour);
                 $('.' + day + '-calendar').find('.hour-' + startHour).append('<em>' + description + '</em>');
             });
         });
