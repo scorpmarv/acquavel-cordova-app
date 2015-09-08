1d7810a5c17818a5576450bb66de7133f7fbdb3e
ScorpMarv <marv1336@gmail.com>
2015-09-08 17:06:56 -0300

    Final version

--- www/js/my-info.js
+++ www/js/my-info.js
@@ -18,6 +18,10 @@
 
     function loadData(){
         console.log(userData);
+        var rev_year = userData.rev_medica.split('-')[0];
+        var rev_month = userData.rev_medica.split('-')[1];
+        var rev_day = userData.rev_medica.split('-')[2];
+        $('#bubbles').append('<p class="speach-left">Recuerde renovar su revisación médica antes del día: '+rev_day+'/'+rev_month+'/'+rev_year+'</p><div class="clear"></div>');
         $.each(userData.planes, function (key,value){
             var today = new Date();
             var year = value.final.split('-')[0];
@@ -25,8 +29,7 @@
             var day = value.final.split('-')[2];
             var theDate = new Date(year,month-1,day);
             var diffDates = (theDate-today)/1000/60/60/24;
-            console.log(diffDates<15);
-            $('#bubbles').append('<p class="speach-left blue-bubble">Te quedan '+value.clases_restantes+' clases</p><div class="clear"></div>');
+            $('#bubbles').append('<p class="speach-left blue-bubble">Te quedan '+value.clases_restantes+' clases en el plan '+value.plan.descripcion+'</p><div class="clear"></div>');
             if (diffDates <= 15) {
                 $('#bubbles').append('<p class="speach-left green-bubble">Recuerde que su plan vence el dia '+day+'/'+month+'/'+year+'</p><div class="clear"></div>');
             }
